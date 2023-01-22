cabec2d8a951edb282f00f936aceb0d2c56731bc
ろむねこ <nem@nem.x0.to>
2023-01-22 23:20:49 +0900

    テーブル生成できるようにした

--- script/route_overview.js
+++ script/route_overview.js
@@ -1,5 +1,6 @@
 window.addEventListener('load', function() {
     getTargetSession()
+    getAllSessionEvents()
 })
 
 var target_session = null;
@@ -7,6 +8,7 @@ var target_session = null;
 function getTargetSession() {
     var params = new URLSearchParams(location.search);
     document.getElementById('query-params').innerHTML = params.get('hoge'); //debug
+    target_session = params.get('hoge');
 }
 
 function getAllSessionEvents() {
@@ -18,8 +20,26 @@ function getAllSessionEvents() {
     })
     .then(function (json){
         console.log(json); //debug
+        for(var i = 0; i < json.length; i++){
+            var event_id = json.result[i].eventId;
+            var point_id = json.result[i].point_id;
+            var event_timestamp = json.result[i].timestamp;
 
-        
+            let tableRef = document.getElementById('event-list-table');
+            let newRow = tableRef.insertRow(-1);
+            let newEventIdCell = newRow.insertCell();
+            let newPointIdCell = newRow.insertCell();
+            let newEventTimestampCell = newRow.insertCell();
+            let addEventId = document.createTextNode(event_id);
+            newEventIdCell.appendChild(addEventId);
+            let addPointId = document.createTextNode(point_id);
+            newPointIdCell.appendChild(addPointId);
+            let addEventTimestamp = document.createTextNode(event_timestamp);
+            newEventTimestampCell.appendChild(addEventTimestamp);
+            
+        }
+
+
 
     });
 }
